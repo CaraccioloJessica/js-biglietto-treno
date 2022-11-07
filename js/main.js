16a927ac2db8b70ccbbb2d5837c7bda12a90cfac
Jessica <jesscara.993@gmail.com>
2022-11-07 15:42:43 +0100

    completed, but tofixed not working

--- js/main.js
+++ js/main.js
@@ -15,16 +15,28 @@ let prezzo = distanza * 0.21
 
 console.log("euro", prezzo);
 
-if (eta < 18){  // SCONTO 20% MINORENNI
+// SCONTO 20% MINORENNI
+if (eta < 18){
   let sconto = (prezzo * 20) / 100;
+  let prezzoScontato = prezzo - sconto;
+  prezzoScontato.toFixed(2);
 
   console.log("sconto euro", sconto)
+
+  document.getElementById("title").innerHTML = `Il prezzo finale è di ${prezzoScontato} euro` 
 }
 
-else if (eta >= 65){  // SCONTO 40% OVER 65
+// SCONTO 40% OVER 65
+else if (eta >= 65){
   let sconto = (prezzo * 40) / 100;
+  let prezzoScontato = prezzo - sconto;
+  prezzoScontato.toFixed(2);
 
   console.log("sconto euro", sconto)
+  
+  document.getElementById("title").innerHTML = `Il prezzo finale è di ${prezzoScontato} euro` 
 }
 
-// OUTPUT
+else {
+  document.getElementById("title").innerHTML = `Il prezzo finale è di ${prezzo} euro`
+}
