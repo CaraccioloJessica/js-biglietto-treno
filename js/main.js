381aa111ce5a85e768f35a61e2f57943693ecf96
Jessica <jesscara.993@gmail.com>
2022-11-07 14:21:38 +0100

    definded price per km with age and km prompt

--- js/main.js
+++ js/main.js
@@ -1,12 +1,19 @@
 // CALCOLO PREZZO BIGLIETTO DEL TRENO
 
 // CHIEDERE ALL'UTENTE KM CHE VUOLE PERCORRERE
+let distanza = parseInt(prompt("Inserisci i km che vuoi percorrere"))
 
+console.log("km", distanza);
 
 // CHIEDERE ALL'UTENTE LA SUA ETA'
+let eta = parseInt(prompt("Inserisci la tua età"))
 
+console.log("anni", eta);
 
 // CALCOLO PREZZO PER KM
+let prezzo = distanza * 0.21
+
+console.log("euro", prezzo);
 
   // SCONTO 20% MINORENNI
 
